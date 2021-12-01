38c30d92fadb65c63beb616c03414ee3907b5263
mickjaer <michael@berner-kjaer.dk>
2021-12-01 14:35:35 +0100

    1 file modified

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Import javascrip files
 import { Reduce2, mapTest } from './testarryfunc.js';
-import { NamesWithEmailSplit } from './NamesWithEmailSplit.js'
+import { NamesWithEmailSplit } from './NamesWithEmailSplit.js';
 import './style.css';
 
 // Write Javascript code!
@@ -8,23 +8,43 @@ const appDiv = document.getElementById('app');
 
 logToHtml(`<h1>JS Tester</h1>`);
 
-// Test dato compare
- logToHtml(`<h3>Test Date compare</h3>`);
-var expireDay = new Date(2020, 4, 12,10,8);
-var toDay = new Date();
-// Wed Apr 29 2020 22:00:00 GMT+0200 (Central European Summer Time)
-logToHtml('toDay: ' + toDay);
-logToHtml('expireDay: ' + expireDay);
-if (expireDay> toDay)
-  logToHtml('ExpireDay is after today');
-if (expireDay< toDay)
-  logToHtml('ExpireDay is before today');
+test1();
 
-if (expireDay === toDay)
-  logToHtml('ExpireDay is equal today');
+async function test1(){
+  await TestPromise('test 1',3000);
+  await TestPromise('test 2',2000);
+  
+}
+
+async function TestPromise(text, number) {
+  logToHtml('TestPromise called: text: '+ text + ' number:' + number );
+  return new Promise(async (resolve, reject) => {
+    await new Promise(resolve => setTimeout(resolve, number));
+    const a = 2 * 7;
+    logToHtml('TestPromise resolve: ' + text);
+    resolve('Result: ', a);
+
+  }
+    ,error=>{reject(error);
+
+  });
+}
+// // Test dato compare
+//  logToHtml(`<h3>Test Date compare</h3>`);
+// var expireDay = new Date(2020, 4, 12,10,8);
+// var toDay = new Date();
+// // Wed Apr 29 2020 22:00:00 GMT+0200 (Central European Summer Time)
+// logToHtml('toDay: ' + toDay);
+// logToHtml('expireDay: ' + expireDay);
+// if (expireDay> toDay)
+//   logToHtml('ExpireDay is after today');
+// if (expireDay< toDay)
+//   logToHtml('ExpireDay is before today');
+
+// if (expireDay === toDay)
+//   logToHtml('ExpireDay is equal today');
 
 //console.log(toDay)
-
 
 // Test Split Name email
 // logToHtml(`<h3>Test Split Name Email function</h3>`);
@@ -32,7 +52,6 @@ if (expireDay === toDay)
 //   arrRes.forEach((e)=>{
 //      logToHtml(e);
 //   })
- 
 
 // Test Map Keys functions
 // logToHtml(`<h1>Test Map Keys function</h1>`);
@@ -46,17 +65,15 @@ if (expireDay === toDay)
 // });
 
 
+
 function logToHtml(text, insertbreak = true) {
   if (text == '') return;
-  
+
   const appDiv = document.getElementById('app');
-  let nytext = "";
-  if (appDiv.innerText != "")
-    nytext = appDiv.innerHTML + text;
-  else
-    nytext = text;
-  if (insertbreak)
-    nytext = nytext + "<br />"
+  let nytext = '';
+  if (appDiv.innerText != '') nytext = appDiv.innerHTML + text;
+  else nytext = text;
+  if (insertbreak) nytext = nytext + '<br />';
 
   appDiv.innerHTML = nytext;
-};
+}
